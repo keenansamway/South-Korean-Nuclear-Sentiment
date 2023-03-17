c29bd08ef298bf36ee3b1a8ad1de8e8717ece0e2
Keenan Samway <70533057+keenansamway@users.noreply.github.com>
2023-03-16 23:50:38 -0400

    Rename file

--- youtube_helper.py
+++ youtube_helper.py
@@ -5,6 +5,12 @@ from config import YOUTUBE_API_KEY
 
 
 class YouTubeHelper:
+    """YouTube Helper Class
+    
+    Attributes:
+        youtube (object): The YouTube API object.
+    
+    """
     def __init__(self, api_key, api_service_name="youtube", api_version="v3"):
         
         assert api_key is not None, "api_key must be provided"
@@ -25,8 +31,8 @@ class YouTubeHelper:
     
 
     def get_search_results(self, query, start_date, end_date, results_to_get=50):
-        """
-        Search for videos based on a query term.
+        """Search for videos based on a query term.
+        
         API Cost: 100 credits for 50 search results
         Documentation: https://developers.google.com/youtube/v3/docs/search/list
         
@@ -92,8 +98,8 @@ class YouTubeHelper:
     #
 
     def _videos(self, **kwargs):
-        """
-        Get the details of a video.
+        """Get the details of a video.
+        
         API Cost: 1 Credit Per Request/Video
         Documentation: https://developers.google.com/youtube/v3/docs/videos/list
         
@@ -104,13 +110,16 @@ class YouTubeHelper:
         Returns:
             A list of videos matching the search query.
         """
-        return self.youtube.videos().list(
+        return self.youtube.videos().list(**kwargs).execute()
+        
+    
+    def get_video_details(self):
+        return self._videos(
             part="snippet,contentDetails,statistics",
-            **kwargs,
-        ).execute()
+        )
     
     
-    def _download_audio_from_id(self, video_id, save_path=None, filename=None):
+    def _download_audio_from_id(self, video_id, save_path, filename):
         video_object = PyTube("https://www.youtube.com/watch?v=" + video_id)
             
         streams = video_object.streams.filter(only_audio=True, file_extension="mp4")
@@ -129,30 +138,32 @@ class YouTubeHelper:
         )
         
     
-    def download_audio(self, video_ids, save_path=None):
-        """
-        Download the audio of a video or list of videos.
+    def download_audio(self, video_ids, save_path="/content/audio"):
+        """Download the audio of a list of videos.
+        
         API Cost: 0 Credits
         Documentation: https://python-pytube.readthedocs.io/en/latest/user/quickstart.html
         
         Parameters:
-            video_ids (str or list): The video ID or list of video IDs to download.
+            video_ids (list): The list of video IDs to download.
             save_path (str): The path to save the audio file to.
             
         Returns:
             None
         """
         assert video_ids is not None, "video_ids must be provided"
-        assert isinstance(video_ids, str) or isinstance(video_ids, list), "video_ids must be a string or list"
+        assert isinstance(video_ids, list), "video_ids must be a list"
         
-        if isinstance(video_ids, list):
-            filenames = []
-            for video_id in video_ids:
-                filenames.append(self._download_audio_from_id(video_id, save_path, video_id+".mp4"))
-            return filenames
-        else:
-            filename = self._download_audio_from_id(video_id, save_path, video_id+".mp4")
-            return filename
+        filenames = []
+        for video_id in video_ids:
+            filename = self._download_audio_from_id(
+                video_id=video_id,
+                save_path=save_path,
+                filename=video_id + ".mp4"
+            )
+            filenames.append(filename)
+            
+        return filenames
     
 
     #
@@ -201,9 +212,10 @@ class YouTubeHelper:
 
 
     def get_video_comments(self, youtube, videoId, top_level_only=False, max_results=100):
-        """
-        Gets the comments of a video.
+        """Gets the comments of a video.
+        
         API Cost: 1 credit for 100 comment threads (top-level comments plus any replies).
+        Documentation: https://developers.google.com/youtube/v3/docs/commentThreads/list
         
         Parameters:
             youtube (object): The YouTube API object.
